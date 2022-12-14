cb61202ca9ae7056a63b7f99ba775527aefb70fb
PARMITA UPADHYAY <94154419+Parmita14@users.noreply.github.com>
2022-12-14 20:28:29 +0530

    Add files via upload

---
+++ reverse_vowels.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String reverseVowels(String s) {
+       int n = s.length();
+        char ch[] = s.toCharArray(); // converting string into character array
+        int start = 0;
+        int end = s.length()-1;
+        while(start<end){
+            if(!isVowel(ch[start])){
+                start++;
+            }else if(!isVowel(ch[end]))
+            {
+                end--;
+            }
+            else
+            {
+                char temp = ch[start];
+                ch[start] = ch[end];
+                ch[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        
+   return String.valueOf(ch);
+    }
+    public static boolean isVowel(char c){
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' ||c == 'E' ||c == 'I' ||c == 'O' ||c == 'U')
+        {
+            return true;
+        }
+        return false;
+    }
+}
